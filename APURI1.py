c7bd630f33dccbc8213c23f16a2eff348427d7e4
バッツ <tendon520@gmail.com>
2025-09-06 17:07:51 +0900

    辞書を追加

--- APURI1.py
+++ APURI1.py
@@ -9,4 +9,13 @@ if 'eitango' in st.session_state and st.session_state.eitango:
 
 eitango = {
     "Hello":"ハロー"
-    ""}
+    "apple":"アップル"
+    "book" : "ブック"
+    "school":"スクール"
+    "friend":"フレンド"
+    "teacher": "ティーチャー"
+    "study":"スタディ"
+    "play":"プレイ"
+    "eat":"イート"
+    "happy":"ハッピー"
+    "beautiful":"ビューティフル"}
