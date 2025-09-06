0f5d9c0dc98d3f2a3903ad6fac050cd0484c5d60
バッツ <tendon520@gmail.com>
2025-09-06 17:39:13 +0900

    翻訳したい英単語を入力する欄と、ボタンを追加

--- APURI1.py
+++ APURI1.py
@@ -2,20 +2,21 @@ import streamlit as st
 
 st.title("English解読マシーン")
 
-if 'eitango' in st.session_state and st.session_state.eitango:
-    st.success(f"{st.session_state.user_name}")
-    st.write("サイドページで翻訳し、表示します。")
+if 'English' not in st.session_state:
+    st.session_state.English = ""
 
+En = st.text_input("翻訳したい英単語を入力してください")
+if st.button("翻訳"):
 
-eitango = {
-    "Hello":"ハロー",
-    "apple":"アップル",
-    "book" : "ブック",
-    "school":"スクール",
-    "friend":"フレンド",
-    "teacher": "ティーチャー",
-    "study":"スタディ",
-    "play":"プレイ",
-    "eat":"イート",
-    "happy":"ハッピー",
-    "beautiful":"ビューティフル"}
+ eitango = {
+     "Hello":"ハロー",
+     "apple":"アップル",
+     "book" : "ブック",
+     "school":"スクール",
+     "friend":"フレンド",
+     "teacher": "ティーチャー",
+     "study":"スタディ",
+     "play":"プレイ",
+     "eat":"イート",
+     "happy":"ハッピー",
+     "beautiful":"ビューティフル"}
