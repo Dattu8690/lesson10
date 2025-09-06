69bb038069ad4b61ba95abeea1aacb2b2d51d2bd
バッツ <tendon520@gmail.com>
2025-09-06 16:54:33 +0900

    if 'eitango' in st.session_state and st.session_state.eitango:     st.success(f"{st.session_state.user_name}")     st.write("サイドページで翻訳し、表示します。")を追加

--- APURI1.py
+++ APURI1.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title("English解読マシーン")
 
-
+if 'eitango' in st.session_state and st.session_state.eitango:
+    st.success(f"{st.session_state.user_name}")
+    st.write("サイドページで翻訳し、表示します。")
 
 
